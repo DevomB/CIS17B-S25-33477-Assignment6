7ef0db6c51519950a382155b5cb689d003fbc8e8
Devom Brahmbhatt <70069338+DevomB@users.noreply.github.com>
2025-06-12 23:27:54 -0700

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,64 @@
-#include "MainWindow.h"
+#include "mainwindow.h"
 #include "ui_mainwindow.h"
+
 #include <QMessageBox>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    connect(ui->addItemButton, &QPushButton::clicked, this, &MainWindow::onAddItemClicked);
-    connect(ui->findItemButton, &QPushButton::clicked, this, &MainWindow::onFindItemClicked);
-    connect(ui->removeItemButton, &QPushButton::clicked, this, &MainWindow::onRemoveItemClicked);
+    QWidget *central = new QWidget(this);
+    QVBoxLayout *mainLayout = new QVBoxLayout(central);
+
+    // Add Item Section
+    QHBoxLayout *addLayout = new QHBoxLayout();
+    idInput = new QLineEdit();
+    descInput = new QLineEdit();
+    locInput = new QLineEdit();
+    QPushButton *addBtn = new QPushButton("Add Item");
+
+    addLayout->addWidget(new QLabel("ID:"));
+    addLayout->addWidget(idInput);
+    addLayout->addWidget(new QLabel("Description:"));
+    addLayout->addWidget(descInput);
+    addLayout->addWidget(new QLabel("Location:"));
+    addLayout->addWidget(locInput);
+    addLayout->addWidget(addBtn);
+
+    // Find Item Section
+    QHBoxLayout *findLayout = new QHBoxLayout();
+    findIdInput = new QLineEdit();
+    QPushButton *findBtn = new QPushButton("Find Item");
+    findLayout->addWidget(new QLabel("Find by ID:"));
+    findLayout->addWidget(findIdInput);
+    findLayout->addWidget(findBtn);
+
+    // Remove Item Section
+    QHBoxLayout *removeLayout = new QHBoxLayout();
+    removeIdInput = new QLineEdit();
+    QPushButton *removeBtn = new QPushButton("Remove Item");
+    removeLayout->addWidget(new QLabel("Remove by ID:"));
+    removeLayout->addWidget(removeIdInput);
+    removeLayout->addWidget(removeBtn);
+
+    // Item List
+    itemList = new QListWidget();
+
+    mainLayout->addLayout(addLayout);
+    mainLayout->addLayout(findLayout);
+    mainLayout->addLayout(removeLayout);
+    mainLayout->addWidget(itemList);
+
+    setCentralWidget(central);
+
+    connect(addBtn, &QPushButton::clicked, this, &MainWindow::onAddItemClicked);
+    connect(findBtn, &QPushButton::clicked, this, &MainWindow::onFindItemClicked);
+    connect(removeBtn, &QPushButton::clicked, this, &MainWindow::onRemoveItemClicked);
 }
 
 MainWindow::~MainWindow() {
@@ -17,9 +66,9 @@ MainWindow::~MainWindow() {
 }
 
 void MainWindow::onAddItemClicked() {
-    QString id = ui->idInput->text();
-    QString desc = ui->descInput->text();
-    QString loc = ui->locInput->text();
+    QString id = idInput->text();
+    QString desc = descInput->text();
+    QString loc = locInput->text();
 
     try {
         auto item = std::make_shared<StoredItem>(id.toStdString(), desc.toStdString(), loc.toStdString());
@@ -28,14 +77,14 @@ void MainWindow::onAddItemClicked() {
     } catch (const std::exception &e) {
         QMessageBox::warning(this, "Error", e.what());
     }
-    ui->idInput->clear();
-    ui->descInput->clear();
-    ui->locInput->clear();
-    ui->idInput->setFocus();
+    idInput->clear();
+    descInput->clear();
+    locInput->clear();
+    idInput->setFocus();
 }
 
 void MainWindow::onFindItemClicked() {
-    QString id = ui->findIdInput->text();
+    QString id = findIdInput->text();
     try {
         auto item = manager.findById(id.toStdString());
         QString msg = QString::fromStdString("Found: " + item->getDescription() + " at " + item->getLocation());
@@ -46,7 +95,7 @@ void MainWindow::onFindItemClicked() {
 }
 
 void MainWindow::onRemoveItemClicked() {
-    QString id = ui->removeIdInput->text();
+    QString id = removeIdInput->text();
     try {
         manager.removeItem(id.toStdString());
         refreshList();
@@ -56,9 +105,9 @@ void MainWindow::onRemoveItemClicked() {
 }
 
 void MainWindow::refreshList() {
-    ui->itemList->clear();
+    itemList->clear();
     for (auto &item : manager.listItemsByDescription()) {
         QString display = QString::fromStdString(item->getId() + " - " + item->getDescription() + " - " + item->getLocation());
-        ui->itemList->addItem(display);
+        itemList->addItem(display);
     }
 }
